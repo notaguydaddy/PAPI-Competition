8c4819377bf1a8b23a73f1ac2d40463228e25a57
SaganashDev <saganashdev@gmail.com>
2021-11-07 15:39:41 +0000

    Editting and compressing times table game

--- system/main/Times Tables.py
+++ system/main/Times Tables.py
@@ -1,123 +1,44 @@
+import random, json, os
 import tkinter as tk
-import random
-import matplotlib.pyplot as plt
-import json
 
-global noofquestions
-noofquestions = []
-
-global accuracy_rates
-accuracy_rates = []
-
-global wrong
+#Globals
+n1, n2, questions, correct = [0 for _ in range(4)]
+numbers = [x for x in range(1, 13)]
 wrong = []
 
 def Start():
-    global n1
-    global n2
-    global ansbox
-    global question
-    global calculation
-    global s
-    
-    n1 = random.choice(numbers)
-    n1 = str(n1)
-        
-    n2 = random.choice(numbers)
-    n2 = str(n2)
-
-    global decider
-
-    decider = random.randint(1, 2)
-
-    if decider == 1:
-        calculation = n1 + " X " + n2
-    
-    elif decider == 2:
-        calculation = str(int(n1) * int(n2)) + " / " + n1
-
-    question.config(text = calculation)
-
-    noofquestions.append("thing")
-
-    accuracy_rates.append((len(something) / len(noofquestions)) * 100)
-
-def end():
-    hmm = tk.Label(text = "Here are all of the calculations you got wrong. Close this window to see your accuracy rate graph!")
-    hmm.pack()
-    for i in range(len(wrong)):
-        tag = tk.Label(text = wrong[i])
-        tag.pack()
+    global n1, n2, question, calculation, ans, calculation
+    n1, n2 = random.choice(numbers), random.choice(numbers) # Chose two random numbers
+    calculation, ans = (str(n1) + " * " + str(n2), n1 * n2) if random.choice([True, False]) else (str(n1 * n2) + " / " + str(n1), n2) # Generate question and answer
+    question.config(text = calculation) # Show question
 
 def Check():
-    userinput = ansbox.get()
-    userinput = int(userinput)
-    if decider == 1:
-        if userinput == int(n1) * int(n2):
-            something.append("something")
-            score.config(text = len(something))
+    global n1, n2, correct, questions
+    try: inans = int(ansbox.get())
+    except:
+        (Start(), ansbox.delete(0, 99))
+        return
+    if inans == ans: (correct := correct + 1, score.config(text = correct))
+    else: (score.config(text = "Incorrect, the correct answer was " + str(int(n1) * int(n2))), wrong.append(calculation))
+    (questions := questions + 1, ansbox.delete(0, 99), Start())
 
-        else:
-            score.config(text = "WRONG. The correct answer was " + str(int(n1) * int(n2)))
-            wrong.append(calculation)
-
-    elif decider == 2:
-        if userinput == int(n1) * int(n2) / int(n1):
-            something.append("something")
-            score.config(text = len(something))
-        else:
-            score.config(text = "WRONG. The correct answer was " + n1)
-            wrong.append(calculation)
-
-    ansbox.delete(0, 99)
-
-    Start()
-
-
+# Game window
 window = tk.Tk()
+(question := tk.Label(text = "Question")).pack()
+(ansbox := tk.Entry()).pack()
+(checkbutton := tk.Button(text = "Check answer", command = Check)).pack()
+(score := tk.Label(text = "Score")).pack()
+(endbutton := tk.Button(text = "End game", command = lambda: window.destroy())).pack()
+(timer := tk.Label()).pack()
+timer.after(60000, lambda: window.destroy())
+(Start(), window.mainloop())
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+# High score calculation
+try: hs = json.load(open("highscore.json"))[0] if os.path.isfile("highscore.json") else 0
+except Exception as e: hs = 0
+if hs < correct: (hs := correct, json.dump([correct], open("highscore.json", "w")))
 
-something = []
-
-running = True
-
-question = tk.Label(text = "Question")
-ansbox = tk.Entry()
-checkbutton = tk.Button(text = "CHECK", command = Check)
-score = tk.Label(text = "Score")
-endbutton = tk.Button(text = "END", command = end)
-timerlabel = tk.Label(text = "")
-
-question.pack()
-ansbox.pack()
-checkbutton.pack()
-score.pack()
-endbutton.pack()
-timerlabel.pack()
-
-timerlabel.after(60000, end)
-
-Start()
-
-window.mainloop()
-
-figure = plt.plot(accuracy_rates)
-plt.xlabel("Questions")
-plt.ylabel("Accuracy rate")
-plt.show()
-
-highscores = []
-newscore = (len(something))
-highscores.append(newscore)
-highscoreFile = open('highscore.txt', 'w+')
-json.dump(highscores, highscoreFile)
-highscoreFile.close()
-
+# Show details
 another_window = tk.Tk()
-
-high_score_thing = tk.Label(text = "See your high scores in highscore.txt!")
-
-high_score_thing.pack()
-
+[tk.Label(text=x).pack() for x in ["{}/{}".format(correct, questions), "High score: {}".format(hs)] + (["Here's what you got wrong:", *wrong] if len(wrong) > 0 else [])]
 another_window.mainloop()
